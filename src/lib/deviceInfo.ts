fde1e9f8b30a7fdbc478ba237bd9181eda1acbf4
hlaing-dev <hlaingmin1235145@gmail.com>
2025-05-08 23:44:06 +0700

    update

--- src/lib/deviceInfo.ts
+++ src/lib/deviceInfo.ts
@@ -13,7 +13,7 @@ import FingerprintJS from '@fingerprintjs/fingerprintjs';
 // }
 
 // Application version - single source of truth
-const APP_VERSION = '1.1.6.3';
+const APP_VERSION = '1.1.6.4';
 
 /**
  * Generate a UUID v4
