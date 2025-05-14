8d7088a50e2de504ab8d311676deeab11e44ad96
hlaing-dev <hlaingmin1235145@gmail.com>
2025-05-15 00:10:12 +0700

    update

--- src/lib/deviceInfo.ts
+++ src/lib/deviceInfo.ts
@@ -14,7 +14,7 @@ interface DeviceInfo {
 }
 
 // Application version - single source of truth
-export const APP_VERSION = '1.1.6.7';
+export const APP_VERSION = '1.1.6.9';
 
 /**
  * Generate a UUID v4
